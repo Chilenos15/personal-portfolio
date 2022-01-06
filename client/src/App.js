763545b0c2d7677659548fc7681393ec4daba6c9
Freddy Ramos <Freddy Ramos>
2022-01-06 16:29:41 -0700

    finished styling

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,9 @@
 import { Route, Routes } from 'react-router-dom';
 import Home from './components/shared/Home';
 import About from './components/shared/About';
+import Skills from './components/shared/Skills'
+import Portfolio from './components/shared/Portfolio'
+import Work from './components/shared/Work'
 import NoMatch from './components/shared/NoMatch';
 import Contacts from './components/contacts/Contacts';
 import Navbar from './components/shared/Navbar';
@@ -12,7 +15,10 @@ const App = () => (
     <Routes>
       <Route exact path='/' element={<Home />} />
       <Route exact path='/home' element={<Home />} />
-      <Route exact path='/about' element={<About />} />
+      <Route exact path='/about' element={<About />} />      
+      <Route exact path='/skills' element={<Skills />} />      
+      <Route exact path='/portfolio' element={<Portfolio />} />      
+      <Route exact path='/work' element={<Work />} />
       <Route exact path='/contacts' element={<Contacts />} />
       <Route exact path='/contacts/:contactId' element={<ContactShow />} />
       <Route path='*' element={<NoMatch />} />
--- client/src/components/shared/Navbar.js
+++ client/src/components/shared/Navbar.js
@@ -1,23 +1,38 @@
 import { Link } from 'react-router-dom';
+import { Navbar, Container } from 'react-bootstrap'; 
 
-const Navbar = ({}) => {
+const MainNavbar = ({}) => {
   return (
     <>
-      <nav>
-        <ul>
-          <Link to='/'>
-            <li>Home</li>
-          </Link>
-          <Link to='/about'>
-            <li>About</li>
-          </Link>
-          <Link to='/contacts'>
-            <li>Contact</li>
-          </Link>
-        </ul>
-      </nav>
+      <Navbar bg="dark" variant="dark">
+        <Container>
+          <Navbar.Brand href="/">
+              Home
+          </Navbar.Brand>
+          <Navbar.Brand href="/about">
+              About
+          </Navbar.Brand>
+          <Navbar.Brand href="/skills">
+              Skills
+          </Navbar.Brand>
+          <Navbar.Brand href="/portfolio">
+              Portfolio
+          </Navbar.Brand>
+          <Navbar.Brand href="/work">
+              Work
+          </Navbar.Brand>
+          <Navbar.Brand href="/contacts">
+              Contact
+          </Navbar.Brand>
+          <Navbar.Toggle />
+          <Navbar.Collapse className="justify-content-end">
+            <Navbar.Text>
+            </Navbar.Text>
+          </Navbar.Collapse>
+        </Container>
+      </Navbar>
     </>
   )
 }
 
-export default Navbar;
+export default MainNavbar;
